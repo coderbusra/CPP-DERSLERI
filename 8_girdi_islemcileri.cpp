7bc2c47ba5d1c4059f561124a64f7ce209fcacb5
busradertli <busradertli000@gmail.com>
2023-07-25 15:18:58 +0300

    Create 8_girdi_islemcileri.cpp

---
+++ 8_girdi_islemcileri.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+    string isim;
+    string soyad;
+    cout<<"Lutfen isim giriniz:";
+    cin>>isim;
+    cout<<"Lutfen soyad giriniz:";
+    cin>>soyad;
+    cout<<"Kullanici adi:"<<isim<<endl;
+    cout<<"Kullanici soyad:"<<soyad;
+    
+    
+}
