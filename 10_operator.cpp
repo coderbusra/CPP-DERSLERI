d9bf2294497e4a42d2343d5c4d6f99940be65d6b
busradertli <busradertli000@gmail.com>
2023-07-25 15:01:44 +0300

    Create 10_operator.cpp

---
+++ 10_operator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+    int sayiBir=5,sayiIki=2;
+    bool sonuc;
+    sonuc=(sayiBir>=sayiIki); //== eþit midir demek != eþit deðilmidir demek >= büyük eþit mi
+	cout<<sonuc;
+    
+  
+  
+  
+  
+   /*	int sayi=2;
+	sayi+=5 ; // sayi=sayi+5; ikisi ayný þeydir
+	sayi-=5; //sayi=sayi-5;
+	sayi*=2; //sayi=sayi*2
+	//bu iþlem bölmede kullanýlmaz!
+	
+	
+	cout<<sayi;
+	*/
+	
+}
