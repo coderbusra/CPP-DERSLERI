85f2f2ad382ac0d4bb5815b90f8cbe8b2befe202
busradertli <busradertli000@gmail.com>
2023-07-25 15:01:47 +0300

    Create 11_odev.cpp

---
+++ 11_odev.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	
+	int genislik,yukseklik,cevre,alan;
+	cout<<"Lutfen genislik giriniz:";
+	cin>>genislik;
+	cout<<"Lutfen yuksekligi giriniz:";
+	cin>>yukseklik;
+	cevre=2*genislik+2*yukseklik;
+	alan=genislik*yukseklik;
+	cout<<"Dikdortgenin cevresi:"<<cevre<<endl;
+	cout<<"Dikdortgenin alani:"<<alan;
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
