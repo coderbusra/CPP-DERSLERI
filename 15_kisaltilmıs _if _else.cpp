1adf5ff5dc094a987e56c0975f5672a40d354f4d
busradertli <busradertli000@gmail.com>
2023-07-25 15:02:07 +0300

    Create 15_kisaltilmıs _if _else.cpp

---
+++ 15_kisaltilmıs _if _else.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){ //kýsaltýlmýþ þekilde if else tanýmlamaya çalýþacaðýz
+	int saat=13;
+	string sonuc=(saat<12)?"ogleden once":"ogleden sonra"; //: dan sonra deðilse
+		cout<<sonuc;
+	
+}
